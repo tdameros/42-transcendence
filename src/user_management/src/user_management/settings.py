4632adc7da5675eaf8cdcdc4a265c46bff611443
tdameros <tomdamerose@gmail.com>
2023-11-13 19:24:29 +0100

    rename user in user_management

--- src/user_management/src/user_management/settings.py
+++ src/user_management/src/user_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&r*!icx1$(sv7f-sj&ezvjxw+pljt-yz(r6yowfg18ihdu@15k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- src/user_management/src/user_management/urls.py
+++ src/user_management/src/user_management/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user_management.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', hello)
 ]
