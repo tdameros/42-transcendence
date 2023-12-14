d7e30e11c8177dc22177a035c768159de416f062
Ethan Delage <ethandelage04@gmail.com>
2023-12-14 12:59:31 +0100

    Add status field in Tournament model

--- tournament/src/api/models.py
+++ tournament/src/api/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from enum import Enum
 
 
 class Tournament(models.Model):
+    CREATED = 0
+    IN_PROGRESS = 1
+    FINISHED = 2
+
     name = models.CharField(max_length=20)
     max_players = models.IntegerField(default=16)
     registration_deadline = models.DateTimeField(blank=True, null=True)
     is_private = models.BooleanField(default=False)
+    status = models.IntegerField(default=CREATED)
