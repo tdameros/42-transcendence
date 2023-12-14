566d01de36f8905a11d19a04b9b66abbf699f6db
Ethan Delage <ethandelage04@gmail.com>
2023-12-14 14:17:10 +0100

    delete src/tournament

--- src/tournament/src/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Tournament(models.Model):
-    name = models.CharField(max_length=20)
-    max_players = models.IntegerField(default=16)
-    registration_deadline = models.DateTimeField(blank=True, null=True)
-    is_private = models.BooleanField(default=False)
--- src/tournament/src/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from api.views import Tournament
-
-
-urlpatterns = [
-    path('', Tournament.as_view()),
-]
