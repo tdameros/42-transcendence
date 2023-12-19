04330b2228b64720f9d716b562f15b9625c64caf
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-12-19 11:19:38 +0100

    Migrations

--- user_management/src/user/models.py
+++ user_management/src/user/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     elo = models.IntegerField(default=500)
+    password = models.CharField(max_length=20)
+    email = models.EmailField()
