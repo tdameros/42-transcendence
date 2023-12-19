d98bc3ac6227bab6e9ee6a08166c700ae14a06c3
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-12-19 11:22:23 +0100

    Completion of sign up view

--- doc/Documentation.md
+++ doc/Documentation.md
@@ -4,7 +4,7 @@ this documentation details the different endpoints of each microservice.
 
 ## Template
 
-> ### [/template](Template-API-Documentation.md#template)
+> ### [/template](Template_api_Documentation.md#template)
 >
 > #### Create / retrieve template
 
@@ -21,3 +21,4 @@ this documentation details the different endpoints of each microservice.
 ## Tournament
 
 ## User Management
+> ### [/user/signup](User_management.md#signup)
--- user_management/src/user/views.py
+++ user_management/src/user/views.py
@@ -3,16 +3,12 @@ from user.models import User
 from django.views import View
 from django.conf import settings
 from datetime import datetime, timedelta
-from user_management.decode_jwt import authenticate_request
+from user_management.JWTManager import user_exist, JWTManager
 import jwt
 import json
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-
-
-class UserInvalidFormat(Exception):
-    pass
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -21,45 +17,79 @@ class SignUpView(View):
     def post(self, request):
         try:
             json_request = json.loads(request.body.decode('utf-8'))
-            if not self.is_valid_user(json_request):
-                raise UserInvalidFormat()
-            User.objects.create(
-                username=json_request['username']
-            )
-            return JsonResponse(data={'username': f'{json_request['username']}'}, status=200)
-        except (json.JSONDecodeError, UserInvalidFormat):
-            return JsonResponse(data={}, status=400)
+            validation_errors = self.signup_infos_validation(json_request)
+            if not validation_errors:
+                User.objects.create(username=json_request['username'],
+                                    email=json_request['email'],
+                                    password=json_request['password'])
+                user = User.objects.filter(username=json_request['username']).first()
+                refresh_token = JWTManager('refresh').generate_token(user.id)
+                return JsonResponse(data={'refresh_token': refresh_token}, status=201)
+            else:
+                return JsonResponse(data={'errors': validation_errors}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse(data={'errors': ['Invalid JSON format in the request body']}, status=400)
 
-    @staticmethod
-    def is_valid_user(json_request):
+        except Exception as e:
+            return JsonResponse(data={'errors': ['An unexpected error occurred']}, status=500)
+
+    def signup_infos_validation(self, json_request):
+        validation_errors = []
+
         username = json_request.get('username')
-        if not SignUpView.is_valid_username(username):
-            return False
-        return True
+        email = json_request.get('email')
+        password = json_request.get('password')
+        valid_username, error_message_username = self.is_valid_username(username)
+        valid_email, error_message_email = self.is_valid_email(email)
+        valid_password, error_message_password = self.is_valid_password(password)
+
+        if not valid_username:
+            validation_errors.append(error_message_username)
+        if not valid_email:
+            validation_errors.append(error_message_email)
+        if not valid_password:
+            validation_errors.append(error_message_password)
+
+        return validation_errors
 
     @staticmethod
     def is_valid_username(username):
         if username is None:
-            return False
+            return False, "Username empty"
         if len(username) > 20:
-            return False
-        return True
+            return False, f"Username length {len(username)} > 20"
+        users = User.objects.filter(username=username)
+        if users.exists():
+            return False, "Username already taken"
+        return True, None
 
-
-class UserView(View):
     @staticmethod
-    def get(request, user_id):
-        if authenticate_request(request) is None:
-            return JsonResponse(data={}, status=401)
-        users = User.objects.filter(id=user_id)
-        if not users.exists():
-            return JsonResponse(data={}, status=404)
-        user = users.first()
-        data = {
-            'username': user.username,
-            'elo': user.elo,
-        }
-        return JsonResponse(data, status=200)
+    def is_valid_email(email):
+        if email is None:
+            return False, "Email empty"
+        if len(email) > 50:
+            return False, f"Email length {len(email)} > 50"
+        if '@' not in email:
+            return False, "Email missing @"
+        if '.' not in email:
+            return False, "Email missing \".\" character"
+        if email.count('@') > 1:
+            return False, "Email contains more than one @ character"
+        return True, None
+
+    @staticmethod
+    def is_valid_password(password):
+        if password is None:
+            return False, "Password empty"
+        if len(password) < 8:
+            return False, f"Password length {len(password)} < 8"
+        if not any(char.isupper() for char in password):
+            return False, "Password missing uppercase character"
+        if not any(char.isdigit() for char in password):
+            return False, "Password missing digit"
+        if not any(char in "!@#$%^&*()-_+=" for char in password):
+            return False, "Password missing special character"
+        return True, None
 
 
 class EncodeJwtView(View):
