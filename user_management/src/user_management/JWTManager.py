191c7d232907b48c5cdc97333583947180fca31a
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-12-19 11:20:34 +0100

    Using real keys and add a JWT manager

---
+++ user_management/src/user_management/JWTManager.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from django.conf import settings
+from user.models import User
+import jwt
+
+
+def user_exist(user_id):
+    return User.objects.filter(id=user_id).exists()
+
+
+class JWTManager:
+    def __init__(self, token_type, payload=None):
+        if token_type == 'access':
+            self.private_key = settings.ACCESS_KEY
+            self.public_key = settings.ACCESS_KEY
+            self.algorithm = 'HS256'
+            self.expire_minutes_reference = settings.ACCESS_EXPIRATION_MINUTES
+        elif token_type == 'refresh':
+            self.private_key = settings.REFRESH_PRIVATE_KEY
+            self.public_key = settings.REFRESH_PUBLIC_KEY
+            self.algorithm = 'RS256'
+            self.expire_minutes_reference = settings.REFRESH_EXPIRATION_MINUTES
+        else:
+            raise Exception("Invalid token type")
+        self.token_type = token_type
+        self.payload = payload
+
+    def decode_jwt(self, encoded_jwt):
+        try:
+            decoded_payload = jwt.decode(encoded_jwt, self.public_key, algorithms=[self.algorithm])
+            return True, decoded_payload
+        except:
+            return False, None
+
+    def generate_token(self, user_id):
+        now = datetime.utcnow()
+        expiration_time = now + timedelta(minutes=self.expire_minutes_reference)
+        payload = {
+            'user_id': user_id,
+            'exp': expiration_time,
+            'token_type': self.token_type
+        }
+        token = jwt.encode(payload, self.private_key, algorithm=self.algorithm)
+        return token
+
+    def is_authentic_and_valid_request(self, request):
+        encoded_jwt = request.headers.get('Authorization')
+        success, decoded_payload = self.decode_jwt(encoded_jwt)
+        if not success:
+            return False, "Invalid token", None
+        if decoded_payload is None:
+            return False, "Empty payload", None
+        if decoded_payload.get('exp') < datetime.now():
+            return False, "Token expired", None
+        user_id = decoded_payload.get('user_id')
+        if user_id is None:
+            return False, "No user_id in payload", None
+        if user_exist(user_id):
+            return True, None, user_id
+        return False, "User does not exist", None
--- user_management/src/user_management/settings.py
+++ user_management/src/user_management/settings.py
@@ -21,6 +21,46 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&r*!icx1$(sv7f-sj&ezvjxw+pljt-yz(r6yowfg18ihdu@15k'
+ACCESS_KEY = 'django-insecure-&r*!icx1$(sv7f-sj&ezvjxw+pljt-yz(r6yowfg18ihdu@15k'
+REFRESH_PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl88VVar5X6lAlHjj4o4r
+r3WoAQloSNbxjgyUd6dU3z3a8JbLibihyl/LjrfAJXCT39FzBbjcWHw7dnDkBeU0
+xX8pPNESkfJI7wxzkc1WcPk1KMwvy1dTaoCub7fZxNl2oOObdzTGpic8co7VOUqa
+5cJks3MTL/8ipxaf4HVJ4luvcySvPflL1woWO3QfTomL/B/Xnu9fmj2ynn8DptfY
+wJEe4eFA/jx+TP3coPBgs/XYG3stdyislm574U+5QvfRi1uii8jkFgpIxwUnxYbx
+mZW+X8IdGmaUnucNeF1pLZjEIcr7MkzP3zm1auQww71DObGTPaLLJNjTPdP3rWYJ
+mQIDAQAB
+-----END PUBLIC KEY-----"""
+REFRESH_PRIVATE_KEY = """-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCXzxVVqvlfqUCU
+eOPijiuvdagBCWhI1vGODJR3p1TfPdrwlsuJuKHKX8uOt8AlcJPf0XMFuNxYfDt2
+cOQF5TTFfyk80RKR8kjvDHORzVZw+TUozC/LV1NqgK5vt9nE2Xag45t3NMamJzxy
+jtU5SprlwmSzcxMv/yKnFp/gdUniW69zJK89+UvXChY7dB9OiYv8H9ee71+aPbKe
+fwOm19jAkR7h4UD+PH5M/dyg8GCz9dgbey13KKyWbnvhT7lC99GLW6KLyOQWCkjH
+BSfFhvGZlb5fwh0aZpSe5w14XWktmMQhyvsyTM/fObVq5DDDvUM5sZM9ossk2NM9
+0/etZgmZAgMBAAECggEAA7hZZ34HGmcFZB4KR5UAUQ5rDYtzeMV7qeV5Be2d0NKt
+ONQZLMjPfiwWyuqJ1OELXqK9VNiQ3cI9msttaw+Q2X4iPpFJCTaMvv7pHhuQELiq
+qtNGII+FRcjdfXNd7Mr/czXgq20pqQyxGIWTiBHh9dtrqFIbmEOCT+HoDRISu29G
+EjBetmvVZFUOcnRsbleYC1SQkzSRQfF8r49EhKMUdjm4UqWoMXlZhyShS5skiTwx
+1I12L52YkDLvzldUnFbB8FJwr7QaWO+WjhTCaIzWhTicL1SQKPY4OGvgc/MsgZg9
+K6bLz5RDDeshP1sfPcXvX0i7IQyYFyXy3VI0XcRXWQKBgQDQTXLwqjahNQai39pS
+Sv9WWlNcuOvyoLa1dd62jW3/yTBXsucolps5On5R1mz5gWgnzEqkC8RIR2jH8+qc
+pKXo8SlVlPtIF2JX1/eU0W64IXOnP+GmpbhL+9J2wr9FJOH7g0krjJ3hiN2WIgU/
+D89MNNHitIbcL5HrMNDE3mA1MwKBgQC6kgR9ajqRGiGc4VwhX/08f5bvnhmrD2f2
+7z+w5ngEwNVfg3adKn4KpwkyIDOt6W5GqnsE8aTDBru9SarHO0W/8Stx7CgkRwjw
+WVL+0cir6SDo6EkcoO8hpAV3LOj8stY5LLL+IgFmf3TNl+AfyWu1yeDmITkU79nf
+jzutO/DuAwKBgGmEMgszTgUPRVNQLdmt3/YwPzYi/nKjcqotESpMLkJ5+aETIQFw
+eSTeOoreIcmqAcbXN6AtzboHYk6XgmrjBKAhOZz+oON95PU2k1WxWXKwj1NTiszN
++bOT1qMON7Gg41ByyqfizT8oA4c/qISvT4T85K0AYag7+KC406hGNVn9AoGBAK0X
++chtvSaQSu0k/HgOeYEekud/FCtroLYuJDY4rNMkIRJ7gpmwKb4yWMrDq47HisNP
+OdFNa+JxJc8pQKOVL1I0K22Hf3qg2P88sE6wTXCJWzobAHHqMdJRPazi4spIFY54
+FRzIaeoxiCmSpaJ4GlFPmjOIUVBGcyoB1okTmqUHAoGAaZvBn10vGBRkK8G3wKYu
+lfXCoz40jhE4N1kidOYBGwPPWyF5OYbhZjCWI2Pq8ldB4hU/bz8eLobrPs4i15Dz
+mZpeFfyRNLuFLV4U8B9TKtLXKqM5ttR+T4qVu6emm9w7sk3XsFpUPHtl6Q8y90qe
+i16cG6lJDF0tN7qwIIDcKYs=
+-----END PRIVATE KEY-----"""
+REFRESH_EXPIRATION_MINUTES = 60 * 24 * 30
+ACCESS_EXPIRATION_MINUTES = 15
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,22 +117,25 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+#SQLite 3 for local development
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'user-management-db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#PostgreSQL for production
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'myuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': 'user-management-db',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
