333df253a3f1d781628c648e9329e2e57871387b
mururoahh <ferraudhugo@gmail.com>
2023-12-28 13:16:04 +0100

    receive player and add them to the queue

--- matchmaking/src/matchmaking_queue/consumers.py
+++ matchmaking/src/matchmaking_queue/consumers.py
@@ -1,20 +1,30 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class QueueConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
 
-        self.send(text_data=json.dumps({
-            'message': 'hey'
+class QueueConsumer(AsyncWebsocketConsumer):
+    queue = []
+
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({
+            'message': 'connection established',
         }))
 
     def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
-        print(text_data_json)
-        self.send(text_data=json.dumps(text_data_json))
+        print('received message: ', text_data_json);
+        await self.send(text_data=json.dumps({
+            'message': 'in queue',
+        }))
+        self.queue.append({
+            'user_id': text_data_json.get('user_id'),
+            'elo': text_data_json.get('elo'),
+        })
+        for user in self.queue:
+            print(user)
--- matchmaking/src/matchmaking_queue/routing.py
+++ matchmaking/src/matchmaking_queue/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/queue/', consumers.QueueConsumer.as_asgi()),
+    re_path(r'ws/queue/$', consumers.QueueConsumer.as_asgi()),
 ]
