9f6e34279d3291a2a4a345e1f55f88035653a1cd
mururoahh <ferraudhugo@gmail.com>
2024-01-02 21:52:31 +0100

    implement channel layers

--- matchmaking/src/matchmaking/settings.py
+++ matchmaking/src/matchmaking/settings.py
@@ -94,6 +94,11 @@ DATABASES = {
 #     }
 # }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- matchmaking/src/matchmaking_queue/consumers.py
+++ matchmaking/src/matchmaking_queue/consumers.py
@@ -2,21 +2,30 @@ import json
 import asyncio
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.layers import get_channel_layer
 
 
 class QueueConsumer(AsyncWebsocketConsumer):
+    counter = 0
+    task_started = False
     queue = []
-    clients = set()
 
     async def connect(self):
         await self.accept()
         await self.send(text_data=json.dumps({
             'message': 'connection established',
         }))
-        asyncio.ensure_future(self.matchmaking())
+        await self.send(text_data=json.dumps({
+            'queue': self.queue,
+        }))
+        print('current queue:')
+        for item in self.queue:
+            print(item)
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        for user in self.queue:
+            if user['channel_name'] == self.channel_name:
+                self.queue.remove(user)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -26,22 +35,39 @@ class QueueConsumer(AsyncWebsocketConsumer):
             'message': 'in queue',
         }))
         self.queue.append({
+            'channel_name': self.channel_name,
             'user_id': text_data_json.get('user_id'),
             'elo': text_data_json.get('elo'),
         })
+        print(f'task_started: {self.task_started}')
+        if len(self.queue) == 1:
+            asyncio.ensure_future(self.matchmaking())
+            self.task_started = True
+        print(f'task_started: {self.task_started}')
         for user in self.queue:
             print(user)
 
+    async def match_found(self, event):
+        await self.send(text_data=json.dumps(event['message']))
+
     async def send_match_notification(self, player1, player2):
-        await self.send(text_data=json.dumps({
-            'message': f'match found: {player1} vs {player2}'
-        }))
-        print(f'match found: {player1} vs {player2}')
+        channel_layer = get_channel_layer()
+        await channel_layer.send(player1['channel_name'], {
+            'type': 'match.found',
+            'message': f"match found: {player1['user_id']} vs {player2['user_id']}",
+        })
+        await channel_layer.send(player2['channel_name'], {
+            'type': 'match.found',
+            'message': f"match found: {player1['user_id']} vs {player2['user_id']}",
+        })
+        print(f"match found: {player1['user_id']} vs {player2['user_id']}")
 
     async def matchmaking(self):
-        while True:
+        while len(self.queue) > 0:
             if len(self.queue) >= 2:
                 player1 = self.queue.pop()
                 player2 = self.queue.pop()
                 await self.send_match_notification(player1, player2)
+            # print(f'counter: {self.counter}')
+            self.counter += 1
             await asyncio.sleep(2)
