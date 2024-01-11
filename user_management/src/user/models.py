6a3d940b1badf2882e9edf2276421c590066c0af
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-01-11 17:15:31 +0100

    add unicity of email for the database

--- user_management/src/user/models.py
+++ user_management/src/user/models.py
@@ -7,4 +7,4 @@ class User(models.Model):
     username = models.CharField(max_length=settings.USERNAME_MAX_LENGTH, unique=True)
     elo = models.IntegerField(default=settings.ELO_DEFAULT)
     password = models.CharField(max_length=settings.PASSWORD_MAX_LENGTH)
-    email = models.EmailField(max_length=settings.EMAIL_MAX_LENGTH)
+    email = models.EmailField(max_length=settings.EMAIL_MAX_LENGTH, unique=True)
