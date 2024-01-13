85ee9e3cefed887492e6d71379874d7eee88d5ae
mururoahh <ferraudhugo@gmail.com>
2024-01-13 15:32:35 +0100

    refactoring authentication

--- matchmaking/src/authenticate.py
+++ matchmaking/src/authenticate.py
@@ -13,10 +13,8 @@ def authenticate_user(token: str) -> Optional[dict]:
     user_id = payload.get('user_id')
     if user_id is None:
         return None
-    # TODO: Uncomment the following lines when the `user/` endpoint is implemented
-    # user = get_user(user_id, token)
-    # return user
-    return payload
+    user = get_user(user_id, token)
+    return user
 
 
 def decode_jwt(token: str) -> Optional[dict]:
@@ -35,8 +33,8 @@ def get_user(user_id: int, token: str) -> Optional[dict]:
     headers = {'Authorization': token}
     try:
         response = requests.get(f'{settings.USER_MANAGEMENT_USER_ENDPOINT}{user_id}', headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        return None
     except Exception:
         return None
+    if response.status_code == 200:
+        return response.json()
+    return None
