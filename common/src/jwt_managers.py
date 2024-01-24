df518cd7b551364bd816b121585ea2a722ece6db
V-Fries <vincentfries@proton.me>
2024-01-24 22:07:28 +0100

    Revamped JWTManager

---
+++ common/src/jwt_managers.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+
+import settings
+
+
+class JWTManager:
+    def __init__(self,
+                 private_key: str | None,
+                 public_key: str | None,
+                 algorithm: str,
+                 expiration_time_minutes: int | None):
+        self.private_key = private_key
+        self.public_key = public_key
+        self.algorithm = algorithm
+        self.expiration_time_minutes = expiration_time_minutes
+
+    def generate_token(self, payload_arg: dict) -> (bool, str | None, list[str] | None):
+        """ returns: Success, jwt, [error messages] """
+
+        now = datetime.now(timezone.utc)
+        expiration_time_minutes = now + timedelta(minutes=self.expiration_time_minutes)
+
+        payload = {'exp': expiration_time_minutes}
+        for key, value in payload_arg.items():
+            payload[key] = value
+
+        try:
+            token = jwt.encode(payload, self.private_key, algorithm=self.algorithm)
+        except Exception as e:
+            return False, None, [str(e)]
+        return True, token, None
+
+    def decode_jwt(self, encoded_jwt: str) -> (bool, dict | None, list[str] | None):
+        """ returns: Success, payload, error message """
+
+        try:
+            decoded_payload = jwt.decode(encoded_jwt, self.public_key, algorithms=[self.algorithm])
+            if decoded_payload.get('exp') is None:
+                return False, None, ["No expiration date found"]
+            return True, decoded_payload, None
+        except Exception as e:
+            return False, None, [str(e)]
+
+
+class UserAccessJWTDecoder:
+    JWT_MANAGER = JWTManager(None,
+                             settings.REFRESH_PUBLIC_KEY,
+                             settings.REFRESH_ALGRORITHM,
+                             None)
+
+    @staticmethod
+    def authenticate(encoded_jwt: str) -> (bool, dict, list[str] | None):
+        """ returns: Success, payload, error message """
+
+        success, decoded_payload, error_decode = UserAccessJWTDecoder.JWT_MANAGER.decode_jwt(encoded_jwt)
+        if not success:
+            return False, None, error_decode
+
+        user_id = decoded_payload.get('user_id')
+        if user_id is None or user_id == '':
+            return False, None, ['No user_id in payload']
+
+        return True, decoded_payload, None
--- user_management/src/user_management/JWTManager.py
+++ user_management/src/user_management/JWTManager.py
@@ -1,9 +1,8 @@
-from datetime import datetime, timedelta
-
-import jwt
 from django.conf import settings
-
 from user.models import User
+
+import common.src.settings as common_settings
+from common.src.jwt_managers import JWTManager, UserAccessJWTDecoder
 
 
 def user_exist(user_id: int) -> bool:
@@ -15,61 +14,61 @@ def user_exist(user_id: int) -> bool:
     return User.objects.filter(id=user_id).exists()
 
 
-class JWTManager:
-    def __init__(self, token_type: str, payload: dict = None):
-        if token_type == 'access':
-            self.private_key = settings.ACCESS_KEY
-            self.public_key = settings.ACCESS_KEY
-            self.algorithm = 'HS256'
-            self.expire_minutes_reference = settings.ACCESS_EXPIRATION_MINUTES
-        elif token_type == 'refresh':
-            self.private_key = settings.REFRESH_PRIVATE_KEY
-            self.public_key = settings.REFRESH_PUBLIC_KEY
-            self.algorithm = 'RS256'
-            self.expire_minutes_reference = settings.REFRESH_EXPIRATION_MINUTES
-        else:
-            raise Exception('Invalid token type')
-        self.token_type = token_type
-        self.payload = payload
+class UserRefreshJWTManager:
+    JWT_MANAGER = JWTManager(settings.REFRESH_KEY,
+                             settings.REFRESH_KEY,
+                             'HS256',
+                             settings.REFRESH_EXPIRATION_MINUTES)
 
-    def decode_jwt(self, encoded_jwt: str) -> (bool, dict, str):
-        try:
-            decoded_payload = jwt.decode(encoded_jwt, self.public_key, algorithms=[self.algorithm])
-            return True, decoded_payload, None
-        except Exception as e:
-            return False, None, str(e)
+    @staticmethod
+    def generate_token(user_id: int) -> (bool, str | None, list[str] | None):
+        """ returns: Success, jwt, [error messages] """
 
-    def generate_token(self, user_id: int) -> (bool, str, str):
         if not user_exist(user_id):
-            return False, None, 'User does not exist'
-        try:
-            now = datetime.utcnow()
-            expiration_time = now + timedelta(minutes=self.expire_minutes_reference)
-            payload = {
-                'user_id': user_id,
-                'exp': expiration_time,
-                'token_type': self.token_type
-            }
-            token = jwt.encode(payload, self.private_key, algorithm=self.algorithm)
-        except Exception as e:
-            return False, None, str(e)
-        return True, token, None
+            return False, None, ['User does not exist']
+        return UserRefreshJWTManager.JWT_MANAGER.generate_token({'user_id': user_id})  # Common
 
-    def is_authentic_and_valid_request(self, encoded_jwt: str) -> (bool, list, int):
-        success, decoded_payload, error_decode = self.decode_jwt(encoded_jwt)
-        errors = []
+    @staticmethod
+    def authenticate(encoded_jwt: str) -> (bool, int | None, list[str] | None):
+        """ returns: Success, user_id, [error messages] """
+
+        success, payload, error_list = UserRefreshJWTManager.JWT_MANAGER.decode_jwt(encoded_jwt)  # Common
         if not success:
-            errors.append(error_decode)
-        elif decoded_payload is None or decoded_payload == {}:
-            errors.append('Empty payload')
-        if errors:
-            return False, errors, None
+            return False, None, error_list
 
-        user_id = decoded_payload.get('user_id')
+        user_id = payload.get('user_id')
         if user_id is None or user_id == '':
-            errors.append('No user_id in payload')
+            return False, None, ['No user_id in payload']
         elif not user_exist(user_id):
-            errors.append('User does not exist')
-        if errors:
-            return False, errors, None
-        return True, None, user_id
+            return False, None, ['User does not exist']
+        return True, user_id, None
+
+
+class UserAccessJWTManager:
+    # Never provide the public key as we must use common.UserAccessJWTDecoder to decode
+    JWT_MANAGER = JWTManager(settings.ACCESS_KEY,
+                             None,
+                             common_settings.REFRESH_ALGRORITHM,
+                             settings.ACCESS_EXPIRATION_MINUTES)
+
+    @staticmethod
+    def generate_token(user_id: int) -> (bool, str | None, list[str] | None):
+        """ returns: Success, jwt, [error messages] """
+
+        if not user_exist(user_id):
+            return False, None, ['User does not exist']
+        return UserAccessJWTManager.JWT_MANAGER.generate_token({'user_id': user_id})  # Common
+
+    @staticmethod
+    def authenticate(encoded_jwt: str) -> (bool, str | None, list[str]):
+        """ returns: Success, user_id, [error messages] """
+
+        success, payload, error_decode = UserAccessJWTDecoder.authenticate(encoded_jwt)  # Common
+        if not success:
+            return False, None, error_decode
+
+        user_id = payload['user_id']
+        if not user_exist(user_id):
+            return False, None, ['User does not exist']
+
+        return True, user_id, None
