47610a3dc7111e5fef39e757661388e1b1625cd9
V-Fries <vincentfries@proton.me>
2024-01-25 15:35:19 +0100

    Made PADDLE_X_POSITION adapt to BOARD_SIZE automaticaly

--- pong_server/src/game_server/settings.py
+++ pong_server/src/game_server/settings.py
@@ -8,6 +8,6 @@ BOARD_SIZE = numpy.array([20., 27.5, 0.])
 
 PADDLE_SIZE = numpy.array([1., 5., 1.])
 PADDLE_MOVE_SPEED = 9.
-PADDLE_X_POSITION = 9.25
+PADDLE_X_POSITION = BOARD_SIZE[0] * 0.5 - PADDLE_SIZE[0] * 0.75
 
 BALL_RADIUS = 1.
