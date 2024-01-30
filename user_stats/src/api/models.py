eb006260174c0e73bb40ae3f1a0c29dbbaf4a1f7
Mururoahh <ferraudhugo@gmail.com>
2024-01-31 00:32:39 +0100

    implement `/match/` and `/user/{id}/history/` endpoints

--- user_stats/src/api/models.py
+++ user_stats/src/api/models.py
@@ -10,3 +10,16 @@ class User(models.Model):
     games_lost = models.IntegerField(default=settings.GAMES_LOST_DEFAULT)
     win_rate = models.FloatField(default=settings.WIN_RATE_DEFAULT)
     friends = models.IntegerField(default=settings.FRIENDS_DEFAULT)
+
+
+class Match(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='user')
+    opponent = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='opponent')
+    user_score = models.IntegerField()
+    opponent_score = models.IntegerField()
+    result = models.BooleanField()
+    user_elo = models.IntegerField()
+    user_elo_delta = models.IntegerField()
+    user_win_rate = models.FloatField()
+    user_expected_result = models.FloatField()
+    date = models.DateTimeField()
--- user_stats/src/api/urls.py
+++ user_stats/src/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from api.views.user import UserView
+from api.views.match import MatchView
+from api.views.user_history import UserHistoryView
 
 urlpatterns = [
-    path('<int:user_id>', UserView.as_view(), name='user'),
+    path('user/<int:user_id>/', UserView.as_view(), name='user'),
+    path('user/<int:user_id>/history/', UserHistoryView.as_view(), name='user_history'),
+    path('match/', MatchView.as_view(), name='match'),
 ]
