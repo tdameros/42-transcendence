e2fa4bbd3cfa10312b1f6431537c2087af979591
V-Fries <vincentfries@proton.me>
2024-02-01 22:28:41 +0100

    Locked the paddles movement to stay within their bounding box

---
+++ pong_server/src/game_server/Scene/Player/Paddle.py
@@ -0,0 +1,86 @@
+import numpy
+
+from src.game_server import settings
+from src.game_server.Scene.Segment2 import Segment2
+from src.game_server.vector_to_dict import vector_to_dict
+
+
+class Paddle(object):
+    def __init__(self, position: numpy.ndarray, player_position: numpy.ndarray):
+        self._position: numpy.ndarray = position.copy()
+        self._movement: numpy.ndarray = numpy.array([0., 0., 0.])
+
+        self._paddle_is_on_the_right: bool = self._position[0] > 0.
+
+        self._player_position: numpy.ndarray = player_position
+
+        self._set_collision_segments()
+
+    def to_json(self) -> dict:
+        return {
+            'size': vector_to_dict(settings.PADDLE_SIZE),
+            'position': vector_to_dict(self._position),
+            'movement': vector_to_dict(self._movement),
+            'move_speed': settings.PADDLE_MOVE_SPEED
+        }
+
+    def update_position(self, time_delta: float):
+        self._position += self._movement * time_delta
+
+        if self._position[1] <= settings.PADDLE_BOUNDING_BOX.get_y_min():
+            self._position[1] = settings.PADDLE_BOUNDING_BOX.get_y_min()
+        elif self._position[1] >= settings.PADDLE_BOUNDING_BOX.get_y_max():
+            self._position[1] = settings.PADDLE_BOUNDING_BOX.get_y_max()
+
+        self._set_collision_segments()
+
+    def set_direction(self, direction: str):
+        if direction == 'up':
+            self._movement[1] = settings.PADDLE_MOVE_SPEED
+        elif direction == 'down':
+            self._movement[1] = -settings.PADDLE_MOVE_SPEED
+        else:
+            self._movement[1] = 0.
+
+    def get_movement(self) -> numpy.ndarray:
+        return self._movement
+
+    def set_position(self, position: numpy.ndarray):
+        self._position = position.copy()
+
+        self._set_collision_segments()
+
+    def get_position(self):
+        return self._position
+
+    def _set_collision_segments(self):
+        x_left = (self._player_position[0] + self._position[0]
+                  - settings.PADDLE_SIZE[0] * 0.5)
+        x_right = (self._player_position[0] + self._position[0]
+                   + settings.PADDLE_SIZE[0] * 0.5)
+
+        y_top = (self._player_position[1] + self._position[1]
+                 + settings.PADDLE_SIZE[1] * 0.5)
+        y_bottom = (self._player_position[1] + self._position[1]
+                    - settings.PADDLE_SIZE[1] * 0.5)
+
+        top_left = numpy.array([x_left, y_top])
+        top_right = numpy.array([x_right, y_top])
+        bottom_right = numpy.array([x_right, y_bottom])
+        bottom_left = numpy.array([x_left, y_bottom])
+
+        self._top_collision_segment = Segment2(top_left, top_right)
+        self._bottom_collision_segment = Segment2(bottom_left, bottom_right)
+        if self._paddle_is_on_the_right:
+            self._front_collision_segment = Segment2(top_left, bottom_left)
+        else:
+            self._front_collision_segment = Segment2(top_right, bottom_right)
+
+    def get_top_collision_segment(self):
+        return self._top_collision_segment
+
+    def get_front_collision_segment(self):
+        return self._front_collision_segment
+
+    def get_bottom_collision_segment(self):
+        return self._bottom_collision_segment
--- pong_server/src/game_server/settings.py
+++ pong_server/src/game_server/settings.py
@@ -1,13 +1,19 @@
 import numpy
 
-AUTHORIZED_DELAY = 0.1
+from src.game_server.Scene.bounding_boxes import (BallBoundingBox,
+                                                  PaddleBoundingBox)
 
-PLAYER_MOVE_SPEED = 15.
+AUTHORIZED_DELAY: float = 0.1
 
-BOARD_SIZE = numpy.array([20., 27.5, 0.])
+PLAYER_MOVE_SPEED: float = 15.
 
-PADDLE_SIZE = numpy.array([1., 5., 1.])
-PADDLE_MOVE_SPEED = 9.
-PADDLE_X_POSITION = BOARD_SIZE[0] * 0.5 - PADDLE_SIZE[0] * 0.75
+BOARD_SIZE: numpy.ndarray = numpy.array([20., 27.5, 0.])
 
-BALL_RADIUS = 1.
+BALL_RADIUS: float = 1.
+BALL_ACCELERATION: float = 1.1
+BALL_BOUNDING_BOX: BallBoundingBox = BallBoundingBox()
+
+PADDLE_SIZE: numpy.ndarray = numpy.array([1., 5., 1.])
+PADDLE_MOVE_SPEED: float = 9.
+PADDLE_X_POSITION: float = BOARD_SIZE[0] * 0.5 - BALL_RADIUS * 1.5 - PADDLE_SIZE[0] * 0.5
+PADDLE_BOUNDING_BOX: PaddleBoundingBox = PaddleBoundingBox()
