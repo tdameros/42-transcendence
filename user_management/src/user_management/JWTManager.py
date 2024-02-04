cfdc43ad2bdf3b7c9a7982d01537d01b50029748
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-02-04 19:29:35 +0100

    small util function added

--- user_management/src/user_management/JWTManager.py
+++ user_management/src/user_management/JWTManager.py
@@ -1,7 +1,11 @@
+import base64
+import json
+
+from django.conf import settings
+from django.http import HttpRequest
+
 import common.src.settings as common_settings
 from common.src.jwt_managers import JWTManager, UserAccessJWTDecoder
-from django.conf import settings
-
 from user.models import User
 
 
@@ -42,6 +46,15 @@ class UserRefreshJWTManager:
         elif not user_exist(user_id):
             return False, None, ['User does not exist']
         return True, user_id, None
+
+
+def get_user_id(request: HttpRequest) -> int:
+    jwt = request.headers.get('Authorization')
+    split_jwt = jwt.split('.')
+    payload = base64.b64decode(split_jwt[1] + '===')
+
+    payload_dict = json.loads(payload)
+    return int(payload_dict['user_id'])
 
 
 class UserAccessJWTManager:
