d6c47015cfcc86d46954c5a575eefbe70f1ec7c3
Ethan Delage <ethandelage04@gmail.com>
2024-02-06 22:24:05 +0100

    start authorization token check in query string

--- notification/src/api/consumers.py
+++ notification/src/api/consumers.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import parse_qs
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -6,7 +7,14 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name = 'public_room'
+        query_string = self.scope['query_string']
+        query_params = parse_qs(query_string.decode())
+        print(query_params)
+        if 'Authorization' not in query_params:
+            print('no auth')
+            await self.close()
+        self.user_id = 0
+        self.group_name = '0'
         await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
@@ -14,10 +22,11 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        if hasattr(self, 'group_name'):
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name
+            )
 
     async def send_notification(self, event):
         await self.send(text_data=json.dumps({'message': event['message']}))
