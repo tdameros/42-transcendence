7aad0d05d36f98d33ddd0508ff7d7880422564a6
mururoahh <ferraudhugo@gmail.com>
2024-02-09 08:52:24 +0100

    Add `DELETE` method to `/friends/` endpoint

--- user_management/src/user/views/friends.py
+++ user_management/src/user/views/friends.py
@@ -1,21 +1,21 @@
 import json
+from typing import Any, Optional
 
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from common.src.jwt_managers import user_authentication
-from user_management.JWTManager import get_user_id
-from typing import Any, Optional
 
-from user.models import User, Friend
+from common.src.jwt_managers import user_authentication
+from user.models import Friend, User
+from user_management.JWTManager import get_user_id
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-@method_decorator(user_authentication(['GET', 'POST']), name='dispatch')
+@method_decorator(user_authentication(['GET', 'POST', 'DELETE']), name='dispatch')
 class FriendsView(View):
     @staticmethod
-    def get(request):
+    def get(request: HttpRequest):
         user_id = get_user_id(request)
         try:
             friends = Friend.objects.filter(user_id=user_id)
@@ -32,14 +32,13 @@ class FriendsView(View):
         return JsonResponse(data=body, status=200)
 
     @staticmethod
-    def post(request):
+    def post(request: HttpRequest):
         user_id = get_user_id(request)
-
         try:
             json_body = json.loads(request.body.decode('utf-8'))
         except json.JSONDecodeError:
             return JsonResponse(data={'errors': 'Invalid JSON format in the request body'}, status=400)
-        valid, errors = FriendsView.validate_post_request(json_body)
+        valid, errors = FriendsView.validate_friend_request(json_body)
         if not valid:
             return JsonResponse(data={'errors': errors}, status=400)
 
@@ -50,6 +49,24 @@ class FriendsView(View):
         if not valid:
             return JsonResponse(data={'errors': [error]}, status=400)
         return JsonResponse(data={'message': 'friend request sent'}, status=200)
+
+    @staticmethod
+    def delete(request: HttpRequest):
+        user_id = get_user_id(request)
+
+        try:
+            json_body = json.loads(request.body.decode('utf-8'))
+        except json.JSONDecodeError:
+            return JsonResponse(data={'errors': 'Invalid JSON format in the request body'}, status=400)
+        valid, errors = FriendsView.validate_friend_request(json_body)
+        if not valid:
+            return JsonResponse(data={'errors': errors}, status=400)
+
+        try:
+            FriendsView.process_delete_request(json_body, user_id)
+        except Exception as e:
+            return JsonResponse(data={'errors': [f'An unexpected error occurred : {e}']}, status=500)
+        return JsonResponse(data={'message': 'friend deleted'}, status=200)
 
     @staticmethod
     def process_post_request(json_body: Any, user_id: int) -> (bool, Optional[list[str]]):
@@ -68,7 +85,19 @@ class FriendsView(View):
         return True, None
 
     @staticmethod
-    def validate_post_request(json_body: Any) -> (bool, Optional[list[str]]):
+    def process_delete_request(json_body: Any, user_id: int) -> (bool, Optional[list[str]]):
+        friend_id = json_body['friend_id']
+        user_friendship = Friend.objects.filter(user_id=user_id, friend_id=friend_id)
+        related_friendship = Friend.objects.filter(user_id=friend_id, friend_id=user_id)
+        if user_friendship.exists():
+            user_friendship = user_friendship.first()
+            user_friendship.delete()
+        if related_friendship.exists():
+            related_friendship = related_friendship.first()
+            related_friendship.delete()
+
+    @staticmethod
+    def validate_friend_request(json_body: Any) -> (bool, Optional[list[str]]):
         errors = []
         friend_id = json_body.get('friend_id')
 
