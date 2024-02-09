6860b6bb516da5bace47c7cf752fccf8e88cb541
V-Fries <vincentfries@proton.me>
2024-02-09 23:35:30 +0100

    Fixed pep8

--- pong_server/src/game_creator/api/error_messages.py
+++ pong_server/src/game_creator/api/error_messages.py
@@ -10,7 +10,8 @@ GAME_ID_FIELD_IS_NOT_AN_INTEGER = 'game_id field is not an integer'
 PLAYERS_FIELD_MISSING = 'players field is missing'
 PLAYERS_FIELD_IS_NOT_A_LIST = 'players field is not a list'
 NOT_ENOUGH_PLAYERS = 'Need at least 2 players to create a game'
-NEED_AT_LEAST_2_PLAYERS_THAT_ARENT_NONE = "Need at least 2 players that aren't None to create a game"
+NEED_AT_LEAST_2_PLAYERS_THAT_ARENT_NONE = ("Need at least 2 players that "
+                                           "aren't None to create a game")
 LEN_PLAYERS_IS_NOT_A_POWER_OF_2 = 'len(players) should be a power of 2'
 BOTH_PLAYERS_ARE_NONE = 'Each pair of players should have at least one non-None player'
 
