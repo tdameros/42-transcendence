aed8db60abefdf3ddad269583278ac580d72cfbb
V-Fries <vincentfries@proton.me>
2024-02-09 17:36:48 +0100

    Added a ClientManager and removed the now useless Game class

---
+++ pong_server/src/game_server/ClientManager.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+import rooms
+from Server import Server
+
+
+class ClientManager(object):
+    #            list[user_id]
+    CLIENTS_IDS: list[int] = []
+    #         dict[user_id, sid]
+    _sid_map: dict[int, str] = {}
+    #             dict[sid, user_id]
+    _user_id_map: dict[str, int] = {}
+
+    @staticmethod
+    def init(clients_ids: list[int]):
+        ClientManager.CLIENTS_IDS = clients_ids
+
+    @staticmethod
+    def have_all_players_joined():
+        return len(ClientManager._sid_map) == len(ClientManager.CLIENTS_IDS)
+
+    @staticmethod
+    def get_user_sid(user_id: int) -> Optional[str]:
+        return ClientManager._sid_map.get(user_id)
+
+    @staticmethod
+    def get_user_id(sid: str) -> Optional[int]:
+        return ClientManager._user_id_map.get(sid)
+
+    @staticmethod
+    async def add_newly_connected_user(user_id: int, sid: str):
+        ClientManager._sid_map[user_id] = sid
+        ClientManager._user_id_map[sid] = user_id
+        await Server.sio.enter_room(sid, rooms.ALL_PLAYERS)
+
+    @staticmethod
+    async def remove_disconnected_user(sid: str):
+        user_id = ClientManager._user_id_map[sid]
+        del ClientManager._sid_map[user_id]
+        del ClientManager._user_id_map[sid]
+        await Server.sio.leave_room(sid, rooms.ALL_PLAYERS)
