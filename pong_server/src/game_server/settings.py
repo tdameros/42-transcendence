da8a1261a6be97d5b8d6b54d44e67a6834dcd041
V-Fries <vincentfries@proton.me>
2024-02-09 17:40:01 +0100

    Updated utils and settings

--- pong_server/src/game_server/settings.py
+++ pong_server/src/game_server/settings.py
@@ -1,12 +1,15 @@
 import numpy
 
-from Scene.bounding_boxes import BallBoundingBox, PaddleBoundingBox
+from Game.bounding_boxes import BallBoundingBox, PaddleBoundingBox
 
 AUTHORIZED_DELAY: float = 0.1
 
 PLAYER_MOVE_SPEED: float = 15.
 
 BOARD_SIZE: numpy.ndarray = numpy.array([20., 27.5, 0.])
+MATCH_SIZE: numpy.ndarray = numpy.array([BOARD_SIZE[0] * 2., BOARD_SIZE[1]])
+MATCHES_X_OFFSET: float = MATCH_SIZE[0] / 4.
+MATCHES_Y_OFFSET: float = MATCH_SIZE[1] / 4.
 
 BALL_RADIUS: float = 1.
 BALL_ACCELERATION: float = 1.1
@@ -18,4 +21,4 @@ PADDLE_MOVE_SPEED: float = 9.
 PADDLE_X_POSITION: float = BOARD_SIZE[0] * 0.5 - BALL_RADIUS * 1.5 - PADDLE_SIZE[0] * 0.5
 PADDLE_BOUNDING_BOX: PaddleBoundingBox = PaddleBoundingBox()
 
-POINTS_TO_WIN: int = 5
+POINTS_TO_WIN_MATCH: int = 5
