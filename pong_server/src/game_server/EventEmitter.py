7436dc44a136925182da6191a98328504520e043
V-Fries <vincentfries@proton.me>
2024-02-09 17:36:07 +0100

    Added classes to handle events

---
+++ pong_server/src/game_server/EventEmitter.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+import numpy
+
+import rooms
+from Game.MatchLocation import MatchLocation
+from Game.PlayerLocation import PlayerLocation
+from Server import Server
+from vector_to_dict import vector_to_dict
+
+
+class EventEmitter(object):
+    @staticmethod
+    async def scene(sid: str, player_location: PlayerLocation, scene: dict):
+        await Server.emit('scene', sid, {
+            'scene': scene,
+            'player_location': player_location.to_json()
+        })
+
+    @staticmethod
+    async def update_paddle(player_location: PlayerLocation,
+                            direction: str,
+                            paddle_position: numpy.ndarray,
+                            skip_sid: Optional[str]):
+        await Server.emit('update_paddle', rooms.ALL_PLAYERS, {
+            'player_location': player_location.to_json(),
+            'direction': direction,
+            'position': vector_to_dict(paddle_position)
+        }, skip_sid)
+
+    @staticmethod
+    async def prepare_ball_for_match(match_location: MatchLocation,
+                                     ball_movement: numpy.ndarray,
+                                     ball_start_time: float):
+        await Server.emit('prepare_ball_for_match', rooms.ALL_PLAYERS, {
+            'match_location': match_location.to_json(),
+            'ball_movement': vector_to_dict(ball_movement),
+            'ball_start_time': ball_start_time,
+        })
+
+    @staticmethod
+    async def update_ball(match_location: MatchLocation,
+                          position: numpy.ndarray,
+                          movement: numpy.ndarray):
+        await Server.emit('update_ball', rooms.ALL_PLAYERS, {
+            'match_location': match_location.to_json(),
+            'position': vector_to_dict(position),
+            'movement': vector_to_dict(movement)
+        })
---
+++ pong_server/src/game_server/EventHandler.py
@@ -0,0 +1,81 @@
+import logging
+from typing import Optional
+
+from socketio.exceptions import ConnectionRefusedError
+
+from AntiCheat import AntiCheat
+from ClientManager import ClientManager
+from EventEmitter import EventEmitter
+from Game.GameManager import GameManager
+from Server import Server
+from shared_code.get_json_web_token import get_json_web_token
+from shared_code.get_query_string import get_query_string
+
+
+class EventHandler(object):
+    @staticmethod
+    def init():
+        Server.sio.on('connect')(EventHandler._connect)
+        Server.sio.on('disconnect')(EventHandler._disconnect)
+        Server.sio.on('update_paddle')(EventHandler._update_paddle)
+
+    @staticmethod
+    async def _connect(sid: str, environ, auth):
+        logging.info(f'{sid} connected')
+
+        user_id = get_json_web_token(get_query_string(environ))['user_id']
+        if user_id not in ClientManager.CLIENTS_IDS:
+            raise ConnectionRefusedError('You are not part of this game')
+
+        previous_sid = ClientManager.get_user_sid(user_id)
+        if previous_sid:
+            await Server.sio.disconnect(previous_sid)
+
+        await ClientManager.add_newly_connected_user(user_id, sid)
+        if GameManager.has_game_started():
+            await EventEmitter.scene(sid,
+                                     GameManager.get_player(user_id).get_location(),
+                                     GameManager.get_scene())
+
+    @staticmethod
+    async def _disconnect(sid: str):
+        await ClientManager.remove_disconnected_user(sid)
+
+    @staticmethod
+    async def _update_paddle(sid: str, player_data):
+        if not GameManager.has_game_started():
+            return
+
+        user_id = ClientManager.get_user_id(sid)
+        player = GameManager.get_player(user_id)
+        if player.get_location().is_looser:
+            return
+
+        success, client_paddle_position, direction = (EventHandler
+                                                      ._get_update_paddle_args(player_data))
+        if not success:
+            return
+
+        await AntiCheat.update_paddle_position_and_direction(client_paddle_position,
+                                                             direction,
+                                                             player,
+                                                             sid)
+
+    @staticmethod
+    def _get_update_paddle_args(player_data) -> (bool, Optional[float], Optional[str]):
+        """ Returns success, client_player_position, direction """
+        try:
+            client_paddle_position = player_data['client_paddle_position']
+            if not isinstance(client_paddle_position, (float, int)):
+                return False, None, None
+
+            direction = player_data['direction']
+            if direction not in ('up', 'down', 'none'):
+                return False, None, None
+
+            return (True,
+                    float(client_paddle_position),
+                    direction)
+
+        except Exception:
+            return False, None, None
