1b0bdea231ae95f17e1341b710a6e979fcd4aa68
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-02-09 16:14:43 +0100

    add /user/id-list/ (#185)
    * add id-list
    
    * isort
    
    * better DB call, better response
    
    * flake8
    
    * changed test
    
    * better documentation

--- user_management/src/user/views/user_id.py
+++ user_management/src/user/views/user_id.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
@@ -15,5 +17,22 @@ class UserIdView(View):
             if user is None:
                 return JsonResponse(data={'errors': ['User not found']}, status=404)
             return JsonResponse(data={'id': user.id, 'username': user.username}, status=200)
+        except Exception as e:
+            return JsonResponse(data={'errors': [f'An unexpected error occurred : {e}']}, status=500)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UserIdListView(View):
+    @staticmethod
+    def post(request):
+        try:
+            id_list = json.loads(request.body).get('id_list')
+            if not isinstance(id_list, list):
+                return JsonResponse(data={'errors': ['id_list should be a list']}, status=400)
+
+            users = User.objects.filter(id__in=id_list)
+            response = {user.id: user.username for user in users}
+            return JsonResponse(data=response, status=200, safe=False)
+
         except Exception as e:
             return JsonResponse(data={'errors': [f'An unexpected error occurred : {e}']}, status=500)
