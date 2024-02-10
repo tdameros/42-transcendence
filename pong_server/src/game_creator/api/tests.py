8e394101e8580d0c94c41191888ffc87182f744c
Vincent Fries <91223304+V-Fries@users.noreply.github.com>
2024-02-10 01:10:31 +0100

    Added more checks to 'create_game/' view (#198)
    * Added more checks to 'create_game/' view
    
    * Fixed pep8
    
    * Added the .env that is required to start the pong-server docker
    
    * Game creator now checks that len(players) is 2 when request issuer is 'matchmaking'

--- pong_server/src/game_creator/api/tests.py
+++ pong_server/src/game_creator/api/tests.py
@@ -6,20 +6,24 @@ from typing import Optional
 from django.urls import reverse
 
 import api.error_messages as error_messages
+from api import settings
 from game_creator.TestCaseNoDatabase import TestCaseNoDatabase
 from shared_code import error_messages as shared_error_messages
 
 
 class PostCreateGameTest(TestCaseNoDatabase):
     def setUp(self):
-        self._port_min = os.getenv('PONG_GAME_SERVERS_MIN_PORT')
-        self._port_max = os.getenv('PONG_GAME_SERVERS_MAX_PORT')
-        self._app_path = os.getenv('GAME_SERVER_PATH')
+        self._port_min: Optional[str] = os.getenv('PONG_GAME_SERVERS_MIN_PORT')
+        self._port_max: Optional[str] = os.getenv('PONG_GAME_SERVERS_MAX_PORT')
+        self._app_path: Optional[str] = os.getenv('GAME_SERVER_PATH')
 
     def tearDown(self):
-        os.environ['PONG_GAME_SERVERS_MIN_PORT'] = self._port_min
-        os.environ['PONG_GAME_SERVERS_MAX_PORT'] = self._port_max
-        os.environ['GAME_SERVER_PATH'] = self._app_path
+        if self._port_min:
+            os.environ['PONG_GAME_SERVERS_MIN_PORT'] = self._port_min
+        if self._port_max:
+            os.environ['PONG_GAME_SERVERS_MAX_PORT'] = self._port_max
+        if self._app_path:
+            os.environ['GAME_SERVER_PATH'] = self._app_path
 
     def post_request(self, request_body) -> (dict, int):
         url = reverse('create_game')
@@ -42,213 +46,201 @@ class PostCreateGameTest(TestCaseNoDatabase):
         os.environ['PONG_GAME_SERVERS_MAX_PORT'] = str(max_port)
         os.environ['GAME_SERVER_PATH'] = app_path
 
+    def run_test(self, request_body, expected_body, expected_status):
+        body, status = self.post_request(request_body)
+
+        self.assertEqual(body, expected_body)
+
+        self.assertEqual(status, expected_status)
+
     def test_valid_matchmaking_request(self):
         port = 4242
         self.set_env_var(port)
 
-        request_body = {
+        self.run_test({
             'game_id': 1,
-            'players': [1, None, 45, None],
-            'request_issuer': 'matchmaking'
-        }
-        # TODO maybe disallow None and more than 2 players for matchmaking?
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 201)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['game_server_uri'], f'http://localhost:{port}')
+            'players': [1, 2],
+            'request_issuer': settings.MATCHMAKING
+        }, {
+            'game_server_uri': f'http://localhost:{port}'
+        }, 201)
 
     def test_valid_tournament_request(self):
         port = 4243
         self.set_env_var(port)
 
-        request_body = {
+        self.run_test({
             'game_id': 1,
-            'players': [1, None, 45, None],
-            'request_issuer': 'tournament'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 201)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['game_server_uri'], f'http://localhost:{port}')
+            'players': [1, None, None, 4],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'game_server_uri': f'http://localhost:{port}'
+        }, 201)
 
     def test_invalid_json(self):
-        request_body = 'invalid json'
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.BAD_JSON_FORMAT])
+        self.run_test('invalid json', {
+            'errors': [error_messages.BAD_JSON_FORMAT]
+        }, 400)
 
     def test_missing_game_id(self):
-        request_body = {
+        self.run_test({
             'players': [1, 2],
-            'request_issuer': 'matchmaking'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.GAME_ID_FIELD_MISSING])
+            'request_issuer': settings.MATCHMAKING
+        }, {
+            'errors': [error_messages.GAME_ID_FIELD_MISSING]
+        }, 400)
 
     def test_invalid_game_id(self):
-        request_body = {
+        self.run_test({
             'game_id': 'invalid',
             'players': [1, 2],
-            'request_issuer': 'matchmaking'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.GAME_ID_FIELD_IS_NOT_AN_INTEGER])
+            'request_issuer': settings.MATCHMAKING
+        }, {
+            'errors': [error_messages.GAME_ID_FIELD_IS_NOT_AN_INTEGER]
+        }, 400)
 
     def test_missing_players(self):
-        request_body = {
+        self.run_test({
             'game_id': 1,
-            'request_issuer': 'matchmaking'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.PLAYERS_FIELD_MISSING])
+            'request_issuer': settings.MATCHMAKING
+        }, {
+            'errors': [error_messages.PLAYERS_FIELD_MISSING]
+        }, 400)
 
     def test_invalid_players_not_a_list(self):
-        request_body = {
+        self.run_test({
             'game_id': 1,
             'players': 4,
-            'request_issuer': 'matchmaking'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.PLAYERS_FIELD_IS_NOT_A_LIST])
+            'request_issuer': settings.MATCHMAKING
+        }, {
+            'errors': [error_messages.PLAYERS_FIELD_IS_NOT_A_LIST]
+        }, 400)
 
     def test_invalid_players_content(self):
-        request_body = {
+        self.run_test({
             'game_id': 1,
-            'players': ['invalid', 2],
-            'request_issuer': 'matchmaking'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.player_is_not_an_optional_int(0)])
+            'players': ['invalid', 2, 4, None],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.player_is_not_an_optional_int(0)]
+        }, 400)
 
     def test_invalid_players_duplicate_id_in_content(self):
         player_id = 2
-        request_body = {
+        self.run_test({
             'game_id': 1,
-            'players': [player_id, player_id],
-            'request_issuer': 'matchmaking'
-        }
+            'players': [player_id, player_id, player_id + 1, None],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.player_is_found_multiple_times(player_id)]
+        }, 400)
 
-        body, status = self.post_request(request_body)
+    def test_invalid_players_pair_of_none(self):
+        self.run_test({
+            'game_id': 1,
+            'players': [1, 2, None, None],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.BOTH_PLAYERS_ARE_NONE]
+        }, 400)
 
-        self.assertEqual(status, 400)
+    def test_invalid_players_only_one_valid_id(self):
+        self.run_test({
+            'game_id': 1,
+            'players': [1, None],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.NEED_AT_LEAST_2_PLAYERS_THAT_ARENT_NONE]
+        }, 400)
 
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'],
-                         [error_messages.player_is_found_multiple_times(player_id)])
+    def test_invalid_players_not_enough_players(self):
+        self.run_test({
+            'game_id': 1,
+            'players': [1],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.NEED_AT_LEAST_2_PLAYERS_THAT_ARENT_NONE]
+        }, 400)
+
+    def test_invalid_players_empty_list(self):
+        self.run_test({
+            'game_id': 1,
+            'players': [],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.NEED_AT_LEAST_2_PLAYERS_THAT_ARENT_NONE]
+        }, 400)
+
+    def test_players_len_not_power_of_2(self):
+        self.run_test({
+            'game_id': 1,
+            'players': [1, 2, 3],
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.LEN_PLAYERS_IS_NOT_A_POWER_OF_2]
+        }, 400)
+
+    def test_players_len_not_2_for_matchmaking(self):
+        self.run_test({
+            'game_id': 1,
+            'players': [1, 2, 3, 4],
+            'request_issuer': settings.MATCHMAKING
+        }, {
+            'errors': [error_messages.NEED_2_PLAYERS_FOR_MATCHMAKING]
+        }, 400)
 
     def test_missing_request_issuer(self):
-        request_body = {
+        self.run_test({
             'game_id': 1,
             'players': [1, 2]
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.REQUEST_ISSUER_FIELD_MISSING])
+        }, {
+            'errors': [error_messages.REQUEST_ISSUER_FIELD_MISSING]
+        }, 400)
 
     def test_invalid_request_issuer_not_a_string(self):
-        request_body = {
+        self.run_test({
             'game_id': 1,
             'players': [1, 2],
             'request_issuer': 4
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.REQUEST_ISSUER_IS_NOT_A_STRING])
+        }, {
+            'errors': [error_messages.REQUEST_ISSUER_IS_NOT_A_STRING]
+        }, 400)
 
     def test_invalid_request_issuer_not_valid(self):
-        request_body = {
+        self.run_test({
             'game_id': 1,
             'players': [1, 2],
             'request_issuer': 'nope'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 400)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'], [error_messages.REQUEST_ISSUER_IS_NOT_VALID])
+        }, {
+            'errors': [error_messages.REQUEST_ISSUER_IS_NOT_VALID]
+        }, 400)
 
     def test_no_ports_available(self):
         port = 43523
+        self.set_env_var(port)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('0.0.0.0', port))
 
-        self.set_env_var(port)
-
-        request_body = {
+        self.run_test({
             'game_id': 1,
             'players': [1, 2],
-            'request_issuer': 'tournament'
-        }
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.error_creating_game_server(
+                shared_error_messages.NO_AVAILABLE_PORTS)]
+        }, 500)
 
-        body, status = self.post_request(request_body)
         s.close()
 
-        self.assertEqual(status, 500)
-
-        self.assertEqual(len(body), 1)
-        self.assertEqual(body['errors'],
-                         [error_messages.error_creating_game_server(
-                             shared_error_messages.NO_AVAILABLE_PORTS)])
-
     def test_popen_fails(self):
-        self.set_env_var(6567, app_path='/invalid_path/ewrfewgrebds')
+        invalid_path = '/1nval1d/p@th/'
+        self.set_env_var(6567, app_path=invalid_path)
 
-        request_body = {
+        self.run_test({
             'game_id': 1,
             'players': [1, 2],
-            'request_issuer': 'tournament'
-        }
-
-        body, status = self.post_request(request_body)
-
-        self.assertEqual(status, 500)
-
-        self.assertEqual(len(body), 1)
-        errno = "[Errno 2] No such file or directory: '/invalid_path/ewrfewgrebds'"
-        self.assertEqual(body['errors'],
-                         [error_messages.popen_failed_to_run_command(errno)])
+            'request_issuer': settings.TOURNAMENT
+        }, {
+            'errors': [error_messages.popen_failed_to_run_command(
+                f"[Errno 2] No such file or directory: '{invalid_path}'")]
+        }, 500)
--- pong_server/src/game_creator/api/views.py
+++ pong_server/src/game_creator/api/views.py
@@ -6,7 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from api import error_messages
+from api import error_messages, settings
 from api.GameCreator import GameCreator
 from api.JsonResponseException import JsonResponseException
 
@@ -37,9 +37,11 @@ class CreateGameView(View):
 
         errors = []
 
-        game_id = CreateGameView._get_game_id(json_body, errors)
-        players = CreateGameView._get_players(json_body, errors)
-        request_issuer = CreateGameView._get_request_issuer(json_body, errors)
+        request_issuer: str = CreateGameView._get_request_issuer(json_body, errors)
+        game_id: int = CreateGameView._get_game_id(json_body, errors)
+        players: list[Optional[int]] = CreateGameView._get_players(
+            json_body, errors, request_issuer
+        )
 
         if len(errors) > 0:
             raise JsonResponseException({'errors': errors}, status=400)
@@ -63,9 +65,8 @@ class CreateGameView(View):
             return -1
 
     @staticmethod
-    def _get_players(json_body, errors) -> list[Optional[int]]:
-
-        players = json_body.get('players')
+    def _get_players(json_body, errors, request_issuer: str) -> list[Optional[int]]:
+        players: any = json_body.get('players')
         if players is None:
             errors.append(error_messages.PLAYERS_FIELD_MISSING)
             return []
@@ -74,21 +75,44 @@ class CreateGameView(View):
             errors.append(error_messages.PLAYERS_FIELD_IS_NOT_A_LIST)
             return []
 
-        correct_players: set[int] = set()
-        for index, player in enumerate(players):
-            if player is None:
-                continue
-            try:
-                if not isinstance(player, int):
-                    player = int(player)
-                if player in correct_players:
-                    errors.append(error_messages.player_is_found_multiple_times(player))
-                else:
-                    correct_players.add(player)
-            except ValueError:
-                errors.append(error_messages.player_is_not_an_optional_int(index))
+        if len(players) != 2 and request_issuer == settings.MATCHMAKING:
+            errors.append(error_messages.NEED_2_PLAYERS_FOR_MATCHMAKING)
+
+        if (len(players) & (len(players) - 1) != 0) and len(players) != 0:
+            errors.append(error_messages.LEN_PLAYERS_IS_NOT_A_POWER_OF_2)
+
+        validated_players: set[int] = set()
+        for i in range(0, len(players) - 1, 2):
+            CreateGameView._check_player(players, i, errors, validated_players)
+            CreateGameView._check_player(players, i + 1, errors, validated_players)
+            if players[i] is None and players[i + 1] is None:
+                errors.append(error_messages.BOTH_PLAYERS_ARE_NONE)
+        if len(players) % 2 == 1:
+            CreateGameView._check_player(players, len(players) - 1, errors, validated_players)
+        if len(validated_players) <= 1:
+            errors.append(error_messages.NEED_AT_LEAST_2_PLAYERS_THAT_ARENT_NONE)
 
         return players
+
+    @staticmethod
+    def _check_player(players: list[any],
+                      index: int,
+                      errors: list[str],
+                      validated_players: set[int]):
+        if players[index] is None:
+            return
+
+        try:
+            if not isinstance(players[index], int):
+                players[index] = int(players[index])
+        except ValueError:
+            errors.append(error_messages.player_is_not_an_optional_int(index))
+            return
+
+        if players[index] in validated_players:
+            errors.append(error_messages.player_is_found_multiple_times(players[index]))
+        else:
+            validated_players.add(players[index])
 
     @staticmethod
     def _get_request_issuer(json_body, errors) -> str:
@@ -101,8 +125,7 @@ class CreateGameView(View):
             errors.append(error_messages.REQUEST_ISSUER_IS_NOT_A_STRING)
             return ''
 
-        if (not request_issuer == 'tournament'
-                and not request_issuer == 'matchmaking'):
+        if request_issuer not in (settings.TOURNAMENT, settings.MATCHMAKING):
             errors.append(error_messages.REQUEST_ISSUER_IS_NOT_VALID)
             return ''
 
