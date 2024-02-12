eda08d96c3592d9a8b9156b8df4bf610111371f2
mururoahh <ferraudhugo@gmail.com>
2024-02-12 02:59:50 +0100

    fix pep8 and isort

--- matchmaking/src/main.py
+++ matchmaking/src/main.py
@@ -1,5 +1,5 @@
-from .server import Server
 from .logging import setup_logging
+from .server import Server
 
 if __name__ == '__main__':
     setup_logging()
--- matchmaking/src/player.py
+++ matchmaking/src/player.py
@@ -1,6 +1,8 @@
 from time import time
 
+
 class Player:
+
     def __init__(self, sid, user_id, elo):
         self.sid = sid
         self.user_id = user_id
