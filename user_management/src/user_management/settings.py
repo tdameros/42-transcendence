9dbaa0d1034b57f053d4d513738ee37fe3797224
tdameros <tomdamerose@gmail.com>
2024-02-12 18:31:13 +0100

    update example env

--- user_management/src/user_management/settings.py
+++ user_management/src/user_management/settings.py
@@ -38,7 +38,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '42.lyon.transcendence@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 MAX_USERNAME_SEARCH_RESULTS = 10
 
