7ac3a3b82040555cee24c6a8fc32147ebba107ad
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-02-16 15:11:02 +0000

    APPEND_SLASH set to false (all services) (#232)

--- tournament/src/tournament/settings.py
+++ tournament/src/tournament/settings.py
@@ -18,6 +18,7 @@ from common.src import settings as common_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+APPEND_SLASH = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
