e4c75ad0f28170e421daa57e95c3f00c5e957109
tdameros <tomdamerose@gmail.com>
2024-02-17 16:47:33 +0100

    refactor navbar

---
+++ front/app/src/js/components/navbar/ConnectedNavbar.js
@@ -0,0 +1,118 @@
+import {Component} from '@components';
+import {userManagementClient} from '@utils/api/index.js';
+import {getRouter} from '@js/Router.js';
+
+export class ConnectedNavbar extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const username = userManagementClient.username;
+    return (`
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+              <a class="navbar-brand">42-Transcendence</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent" aria-expanded="false"
+                      aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                          ${this.#generateNavLink('local')}
+                      </li>
+                      <li class="nav-item">
+                          ${this.#generateNavLink('multiplayer')}
+                      </li>
+                      <li class="nav-item">
+                          ${this.#generateNavLink('tournaments')}
+                      </li>
+                  </ul>
+                  <div class="d-flex align-items-center mb-2 mb-lg-0">
+                      <search-nav-component class="ms-2 me-2"></search-nav-component>
+                  </div>
+                  <div id="log-part" class="d-flex align-items-center">
+                      <theme-button-component class="me-1"></theme-button-component>
+                      <notification-nav-component class="me-1"></notification-nav-component>
+                      <div class="dropdown mx-2">
+                                <span class="dropdown-toggle" id="dropdownMenuLink"
+                                      data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img id="nav-profile-img" src="/img/tdameros.jpg" alt="profile image"
+                                         class="rounded-circle"
+                                         style="width: 40px; height: 40px;">
+                                    <span id="nav-username">@${username}</span>
+                                </span>
+                          <ul class="dropdown-menu dropdown-menu-end"
+                              aria-labelledby="dropdownMenuLink">
+                              <li><a class="dropdown-item" onclick="window.router.navigate('/profile/${username}/')">Profil</a></li>
+                              <li><a class="dropdown-item">Settings</a></li>
+                              <li><a id="logout" class="dropdown-item text-danger">Sign out</a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </nav>
+    `);
+  }
+
+  style() {
+    return (`
+      <style>
+
+      </style>
+    `);
+  }
+
+  postRender() {
+    this.local = this.querySelector('#local');
+    this.multiplayer = this.querySelector('#multiplayer');
+    this.tournaments = this.querySelector('#tournaments');
+
+    super.addComponentEventListener(this.local, 'click', this.#navigate);
+    super.addComponentEventListener(this.multiplayer, 'click', this.#navigate);
+    super.addComponentEventListener(this.tournaments, 'click', this.#navigate);
+
+    const disablePaddingTop = this.getAttribute('disable-padding-top');
+    if (disablePaddingTop !== 'true') {
+      const navbarHeight = this.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbarHeight + 'px';
+    } else {
+      document.body.style.paddingTop = '0px';
+    }
+    const logout = this.querySelector('#logout');
+    super.addComponentEventListener(logout, 'click', this.#logout);
+  }
+
+
+  #navigate(event) {
+    getRouter().navigate(`/${event.target.id}/`);
+  }
+
+  #logout() {
+    userManagementClient.logout();
+    getRouter().navigate('/');
+  }
+
+  #generateNavLink(linkId) {
+    const activeLink = this.getAttribute('nav-active');
+    const navLink = document.createElement('a');
+    navLink.setAttribute('id', linkId);
+    navLink.classList.add('nav-link');
+    if (activeLink === linkId) {
+      navLink.classList.add('active');
+    }
+    navLink.text = linkId.charAt(0).toUpperCase() + linkId.slice(1);
+    return (navLink.outerHTML);
+  }
+
+  addNotification(notification) {
+    const notificationNav = this.querySelector('notification-nav-component');
+    if (notificationNav) {
+      notificationNav.addNotification(notification);
+    }
+  }
+}
---
+++ front/app/src/js/components/navbar/SearchNav.js
@@ -0,0 +1,109 @@
+import {Component} from '@components';
+import {userManagementClient} from '@utils/api/index.js';
+import {getRouter} from '@js/Router.js';
+import {ErrorPage} from '@utils/ErrorPage.js';
+
+export class SearchNav extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (`
+      <div class="position-relative z-1 me-2 ms-2">
+          <form id="search-form" class="d-flex" role="search">
+              <input id="search-bar" class="form-control" type="search"
+                     placeholder="Search users..." aria-label="Search" autocomplete="off">
+          </form>
+          <div id="search-results" class="rounded">
+          </div>
+      </div>
+    `);
+  }
+
+  style() {
+    return (`
+      <style>
+      #search-results {
+          position: absolute;
+          width: 100%;
+          max-height: 200px;
+          overflow-y: auto;
+          display: none;
+          z-index: 2; 
+      }
+        
+      .result-item {
+          cursor: pointer;
+          background-color: var(--bs-body-bg);
+          border: 1px solid var(--bs-border-color);
+      }
+      </style>
+    `);
+  }
+
+  postRender() {
+    this.searchBar = this.querySelector('#search-bar');
+    super.addComponentEventListener(
+        this.searchBar, 'input', this.#searchBarHandler,
+    );
+    super.addComponentEventListener(
+        document, 'click', this.#DOMClickHandler,
+    );
+    this.searchResults = this.querySelector('#search-results');
+    this.searchForm = this.querySelector('#search-form');
+    super.addComponentEventListener(
+        this.searchForm, 'submit', this.#searchFormHandler,
+    );
+  }
+
+  async #searchBarHandler(event) {
+    if (event.target.value.length < 3) {
+      this.searchResults.style.display = 'none';
+      return;
+    }
+    try {
+      const {response, body} = await userManagementClient.searchUsername(
+          event.target.value,
+      );
+      this.searchResults.innerHTML = '';
+      if (response.ok) {
+        this.searchResults.innerHTML = this.#renderSearchResults(body['users']);
+        if (body['users'].length > 0) {
+          this.searchResults.style.display = 'block';
+        } else {
+          this.searchResults.style.display = 'none';
+        }
+      }
+    } catch (error) {
+      ErrorPage.loadNetworkError();
+    }
+  }
+
+  #renderSearchResults(users) {
+    return (users.slice(0, 3).map((username) => {
+      return (`
+      <div class="result-item p-1" onclick="window.router.navigate('/profile/${username}/')" username="${username}">
+        <img src="/img/tdameros.jpg" alt="profile image" class="rounded-circle" style="width: 40px; height: 40px;">
+        ${username}
+      </div>
+    `);
+    }).join(''));
+  }
+
+  #DOMClickHandler(event) {
+    if (this.searchResults &&
+      !this.searchResults.contains(event.target) &&
+      event.target !== this.searchBar) {
+      this.searchResults.style.display = 'none';
+    }
+  }
+
+  #searchFormHandler(event) {
+    event.preventDefault();
+    const username = this.searchBar.value;
+    if (username.length > 0) {
+      getRouter().navigate(`/profile/${username}/`);
+    }
+  }
+}
