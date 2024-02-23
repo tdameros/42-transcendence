2d19ac92d769af00a110ae6c4004161e704d5b75
tdameros <tomdamerose@gmail.com>
2024-02-23 23:12:21 +0100

    Add authentication checks for every pages

--- front/app/src/js/components/pages/home/Home.js
+++ front/app/src/js/components/pages/home/Home.js
@@ -1,14 +1,22 @@
 import {Component} from '@components';
+import {userManagementClient} from '@utils/api';
 
 export class Home extends Component {
   constructor() {
     super();
   }
   render() {
-    return (`
+    if (userManagementClient.isAuth()) {
+      return (`
       <navbar-component nav-active="home"></navbar-component>
       <friends-sidebar-component main-component="home-content-component"></friends-sidebar-component>
     `);
+    } else {
+      return (`
+        <navbar-component nav-active="home"></navbar-component>
+        <home-content-component></home-content-component>
+      `);
+    }
   }
   style() {
     return (`
