642bf99d17007c40486dd3dffdfe613de6b841e9
V-Fries <vincentfries@proton.me>
2024-03-02 15:07:45 +0100

    Fixed PlayerManager memory not being shared by all workers by adding a redis cache

--- pong_server/dev_game_creator.sh
+++ pong_server/dev_game_creator.sh
@@ -4,7 +4,6 @@
 export PONG_GAME_SERVERS_MIN_PORT=42200
 export PONG_GAME_SERVERS_MAX_PORT=42210
 export GAME_SERVER_PATH=~/git/transcendence/pong_server/src/game_server/
-
-./test_game_creator.sh || exit $?
+export DEBUG=False
 
 (cd src/game_creator/ && python3 manage.py runserver)
--- pong_server/src/game_creator/api/tests/test_RemovePlayersCurrentGameView.py
+++ pong_server/src/game_creator/api/tests/test_RemovePlayersCurrentGameView.py
@@ -9,11 +9,11 @@ from api.tests.utils.TestCaseNoDatabase import TestCaseNoDatabase
 from common.src.jwt_managers import ServiceAccessJWT
 
 
-class DeleteRemovePlayersCurrentGameViewTest(TestCaseNoDatabase):
-    def delete_request(self,
-                       request_body,
-                       jwt: Optional[str],
-                       reset_player_manager: bool) -> (dict, int):
+class PostRemovePlayersCurrentGameViewTest(TestCaseNoDatabase):
+    def post_request(self,
+                     request_body,
+                     jwt: Optional[str],
+                     reset_player_manager: bool) -> (dict, int):
         url = reverse('remove_players_current_game')
         if jwt is None:
             success, jwt, errors = ServiceAccessJWT.generate_jwt()
@@ -21,9 +21,9 @@ class DeleteRemovePlayersCurrentGameViewTest(TestCaseNoDatabase):
                 raise Exception(f'Failed to generate jwt: {errors}')
 
         if reset_player_manager:
-            PlayerManager._users = {}
+            PlayerManager.clear()
 
-        response = self.client.delete(
+        response = self.client.post(
             url,
             json.dumps(request_body),
             content_type='application/json',
@@ -40,7 +40,7 @@ class DeleteRemovePlayersCurrentGameViewTest(TestCaseNoDatabase):
                  expected_status,
                  jwt: Optional[str] = None,
                  reset_player_manager: bool = True):
-        body, status = self.delete_request(request_body, jwt, reset_player_manager)
+        body, status = self.post_request(request_body, jwt, reset_player_manager)
 
         self.assertEqual(status, expected_status)
         self.assertEqual(body, expected_body)
@@ -48,6 +48,13 @@ class DeleteRemovePlayersCurrentGameViewTest(TestCaseNoDatabase):
     def test_valid_request(self):
         PlayerManager.add_players([1], 4242)
 
+        self.run_test({
+            'players': [1, 2],
+        }, {}, 204)
+
+        self.assertEqual(PlayerManager.get_player_game_port(1), None)
+
+    def test_valid_request_no_players_are_in_a_game(self):
         self.run_test({
             'players': [1, 2],
         }, {}, 204)
