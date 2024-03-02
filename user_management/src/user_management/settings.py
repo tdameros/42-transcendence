966704ad2cec10ad55ba2edcc27a3e7e686c396d
tdameros <tomdamerose@gmail.com>
2024-03-02 02:35:08 +0100

    use databases credentials

--- user_management/src/user_management/settings.py
+++ user_management/src/user_management/settings.py
@@ -210,11 +210,11 @@ else:
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.postgresql',
-                'NAME': 'mydatabase',
-                'USER': 'myuser',
-                'PASSWORD': 'mypassword',
-                'HOST': 'user-management-db',
-                'PORT': '5432',
+                'NAME': os.environ.get('POSTGRES_DB'),
+                'USER': os.environ.get('POSTGRES_USER'),
+                'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+                'HOST': os.environ.get('POSTGRES_HOST'),
+                'PORT': os.environ.get('POSTGRES_PORT'),
             }
         }
 
