b7b48f4c23371fe7c3a5bbb61028567fea2c3994
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-03-02 14:24:54 +0100

    allowing one user to have multiple OAuth services

--- user_management/src/user/models.py
+++ user_management/src/user/models.py
@@ -8,13 +8,6 @@ from user_management import settings
 
 
 class User(models.Model):
-    # the first element of each tuple is the actual value to be stored
-    # second element is the human-readable name for the value
-    OAUTH_CHOICES = [
-        (None, 'None'),
-        ('github', 'github'),
-        ('42api', '42api'),
-    ]
     username = models.CharField(max_length=settings.USERNAME_MAX_LENGTH, unique=True)
     password = models.CharField(max_length=settings.PASSWORD_MAX_LENGTH, null=True)
     email = models.EmailField(max_length=settings.EMAIL_MAX_LENGTH, unique=True)
@@ -30,7 +23,6 @@ class User(models.Model):
     account_deleted = models.BooleanField(default=False)
     last_login = models.DateTimeField(null=True)
     last_activity = models.DateTimeField(default=timezone.now)
-    oauth = models.CharField(max_length=7, choices=OAUTH_CHOICES, default=None, null=True)
 
     def verify_2fa(self, code):
         return pyotp.TOTP(self.totp_secret).verify(code)
@@ -42,11 +34,37 @@ class User(models.Model):
         self.last_login = datetime.now(timezone.utc)
         self.save()
 
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
+
 
 class PendingOAuth(models.Model):
     hashed_state = models.CharField(max_length=settings.OAUTH_STATE_MAX_LENGTH, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     source = models.CharField(max_length=settings.OAUTH_SOURCE_MAX_LENGTH)
+
+
+class UserOAuth(models.Model):
+    # the first element of each tuple is the actual value to be stored
+    # second element is the human-readable name for the value
+    OAUTH_SERVICES = [
+        ('github', 'github'),
+        ('42api', '42api'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='oauths')
+    service = models.CharField(max_length=20, choices=OAUTH_SERVICES)
+    service_id = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "User OAuth"
+        verbose_name_plural = "User OAuths"
+
+    def __str__(self):
+        return f"{self.user.username}'s {self.service} OAuth"
 
 
 class Friend(models.Model):
