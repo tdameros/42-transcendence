3f78a4b28bb1716b1ee7f565540d7720db024fbc
Tom Damerose <50496792+tdameros@users.noreply.github.com>
2024-03-03 22:24:28 +0100

    352 auto redirect player in game (#387)
    * fix pong_server urls
    
    * automatically redirect to a game if one is in progress
    
    * add PongServerClient

--- front/app/src/js/utils/api/index.js
+++ front/app/src/js/utils/api/index.js
@@ -2,15 +2,19 @@ import {UserManagementClient} from './UserManagementClient.js';
 import {UserStatsClient} from './UserStatsClient.js';
 import {TournamentClient} from './TournamentClient.js';
 import {NotificationClient} from './NotificationClient.js';
+import {PongServerClient} from './PongServerClient.js';
 
 export const userManagementClient = new UserManagementClient();
 export const userStatsClient = new UserStatsClient();
 export const tournamentClient = new TournamentClient();
 export const notificationClient = new NotificationClient();
 
+export const pongServerClient = new PongServerClient();
+
 export {
   UserManagementClient,
   UserStatsClient,
   TournamentClient,
   NotificationClient,
+  PongServerClient,
 };
