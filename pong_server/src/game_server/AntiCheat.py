963cb407739a2b7ec6dbeb9cdfc39cb2a7243c77
Vincent Fries <91223304+V-Fries@users.noreply.github.com>
2024-03-09 20:12:40 +0100

    Fixed speed hack (#484)

--- pong_server/src/game_server/AntiCheat.py
+++ pong_server/src/game_server/AntiCheat.py
@@ -1,3 +1,4 @@
+import time
 from typing import Optional
 
 import numpy
@@ -9,6 +10,9 @@ from Game.Player.Player import Player
 
 
 class AntiCheat(object):
+    #                      dict[user_id, last_movement_time]
+    _last_player_movement: dict[int, float] = {}
+
     @staticmethod
     async def update_paddle_position_and_direction(client_paddle_position: float,
                                                    direction: str,
@@ -16,8 +20,9 @@ class AntiCheat(object):
                                                    sid: str):
         paddle: Paddle = player.get_paddle()
 
-        fixed_paddle_position: Optional[float] \
-            = AntiCheat._get_fixed_paddle_position(client_paddle_position, paddle)
+        fixed_paddle_position: Optional[float] = AntiCheat._get_fixed_paddle_position(
+            client_paddle_position, paddle, AntiCheat._get_authorized_delay(player.PLAYER_ID)
+        )
 
         cheat_or_high_latency: bool = fixed_paddle_position is not None
 
@@ -39,13 +44,26 @@ class AntiCheat(object):
                                          skip_sid)
 
     @staticmethod
+    def _get_authorized_delay(player_id: int) -> float:
+        current_time: float = time.time()
+        last_movement_time: float = AntiCheat._last_player_movement.get(player_id, None)
+        if last_movement_time is None:
+            authorized_delay = settings.AUTHORIZED_DELAY
+        else:
+            authorized_delay = min(current_time - last_movement_time,
+                                   settings.AUTHORIZED_DELAY)
+        AntiCheat._last_player_movement[player_id] = current_time
+        return authorized_delay
+
+    @staticmethod
     def _get_fixed_paddle_position(client_paddle_position: float,
-                                   paddle: Paddle) -> Optional[float]:
+                                   paddle: Paddle,
+                                   authorized_delay: float) -> Optional[float]:
         position_on_server: float = paddle.get_position()[1]
         paddle_movement: float = paddle.get_movement()[1]
 
         max_player_position: float = (position_on_server
-                                      + paddle_movement * settings.AUTHORIZED_DELAY)
+                                      + paddle_movement * authorized_delay)
 
         if paddle_movement > 0.:
             if client_paddle_position > max_player_position:
